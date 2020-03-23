f664711c717f319c7522df6b0037eb3c900da986
Viet Anh <vietanh@emotiv.com>
2020-03-23 09:19:26 +0700

    CUI-526: add license to config file (Swift)

--- mobile/ios/CortexV2-Emotiv-iOS/CortexV2-Example-Swift/CortexV2-Example-Swift/Config.swift
+++ mobile/ios/CortexV2-Emotiv-iOS/CortexV2-Example-Swift/CortexV2-Example-Swift/Config.swift
@@ -8,4 +8,5 @@
 
 let ClientId: String = "" //"The client id of your Cortex app goes here"
 let ClientSecret: String = ""//The client secret of your Cortex app goes here"
+let License: String = ""//The license of your Cortex app goes here"
 let TrainingProfileName = "cortex-v2-example"
