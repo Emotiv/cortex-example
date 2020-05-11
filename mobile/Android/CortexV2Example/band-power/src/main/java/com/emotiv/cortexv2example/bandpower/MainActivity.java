1683f40ac515f90b898e9fefae0e14dafabdf3a4
VietNguyenHoang <viet@emotiv.vn>
2020-05-11 14:05:30 +0700

    CUI-608: update control device function for EEG

--- mobile/Android/CortexV2Example/band-power/src/main/java/com/emotiv/cortexv2example/bandpower/MainActivity.java
+++ mobile/Android/CortexV2Example/band-power/src/main/java/com/emotiv/cortexv2example/bandpower/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
