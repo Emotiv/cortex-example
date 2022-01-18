4a5eb9947b8163e541b1728b57b076d17a658803
tungntEmotiv <tungnguyen@emotiv.com>
2022-01-18 16:38:57 +0700

    support insight 2 for unity example (#159)

--- unity/Assets/_brain_visualizer/script/3D/Headset3DController.cs
+++ unity/Assets/_brain_visualizer/script/3D/Headset3DController.cs
@@ -18,7 +18,7 @@ namespace dirox.emotiv.controller
 
         private void setConnectedHeadset (Headset selectedHeadsetInformation)
         {
-            bool isInsightConnected = (selectedHeadsetInformation.HeadsetType == HeadsetTypes.HEADSET_TYPE_INSIGHT);
+            bool isInsightConnected = Utils.IsInsightType(selectedHeadsetInformation.HeadsetType);
 
             insight.SetActive (isInsightConnected);
             epoc.SetActive (!isInsightConnected);
