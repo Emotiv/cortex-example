6aacda3ab1e69fba44d226465f2cd32317dd85ca
tung_tung <tungnguyen@emotiv.com>
2023-06-29 16:39:08 +0700

    update rework

--- python/query_records.py
+++ python/query_records.py
@@ -36,14 +36,14 @@ class Records():
     def start(self):
         """
         To open websocket and process authorizing step.
-        After get authorize_done The program will query_records
+        After get authorize_done The program will query records
         Returns
         -------
         None
         """
         self.c.open()
 
-    def query_records(self):
+    def query_records(self, license_id, application_id):
         """
         To query records
         Parameters
@@ -51,14 +51,12 @@ class Records():
         orderBy : array of object, required : Specify how to sort the records.
         query: object, required: An object to filter the records.
                If you set an empty object, it will return all records created by your application
-               If you want get records created by other applications, you need set licenseId as parameter of query object
+               If you want get records created by other application, you need set licenseId and applicationId as parameter of query object
         More detail at https://emotiv.gitbook.io/cortex-api/records/queryrecords
         Returns
         -------
         """
 
-        license_id = '' #Set this parameter to filter the records by their license.
-        application_id =  "", # filter by applicationId but must set licenseId as well. for example: com.emotiv.emotivpro
         query_obj = {}
         if license_id != '':
             query_obj["licenseId"] = license_id
@@ -85,14 +83,15 @@ class Records():
         """
         self.c.request_download_records(record_ids)
 
-    def export_record(self, record_ids):
+    def export_record(self, record_ids, license_ids):
         """
         To export records
         By default, you can only export the records that were created by your application.
         If you want to export a record that was created by another applications 
-        then you must provide the license id of those applications in the parameter licenseIds.
+        then you must provide the license ids of those applications in the parameter licenseIds.
         Parameters
         record_ids: list, required: list of wanted export record ids
+        license_ids: list, no required: list of license id of other applications 
         ----------
         More detail at https://emotiv.gitbook.io/cortex-api/records/exportrecord
         Returns
@@ -101,13 +100,8 @@ class Records():
         """
         folder = '' # your place to export, you should have write permission, for example: 'C:\\Users\\NTT\\Desktop'
         stream_types = ['EEG', 'MOTION', 'PM', 'BP']
-        export_format = 'CSV'
+        export_format = 'CSV' # support 'CSV' or 'EDF'
         version = 'V2'
-
-        # license_ids: If set empty list, which means that you can only export the records created by your application.
-        #             You can provide the license ID of other applications in order to export the records created by these applications.
-        license_ids = []
-
         self.c.export_record(folder, stream_types, export_format, record_ids, version, licenseIds=license_ids)
 
     
@@ -115,7 +109,7 @@ class Records():
     def on_authorize_done(self, *args, **kwargs):
         print('on_authorize_done')
         # query records 
-        self.query_records()
+        self.query_records(self.license_id, self.application_id)
 
     # callbacks functions
     def on_query_records_done(self, *args, **kwargs):
@@ -125,21 +119,27 @@ class Records():
         # print(data)
         not_downloaded_record_Ids = []
         export_record_Ids = []
+        license_ids = []
         for item in data:
             uuid = item['uuid']
             sync_status = item["syncStatus"]["status"]
             application_id = item["applicationId"]
+            license_id = item["licenseId"]
             print("recordId {0}, applicationId {1}, sync status {2}".format(uuid, application_id, sync_status))
             if (sync_status == "notDownloaded") :
                 not_downloaded_record_Ids.append(uuid)
             elif (sync_status == "neverUploaded") or (sync_status == "downloaded"):
                 export_record_Ids.append(uuid)
+                if license_id not in license_ids:
+                    license_ids.append(license_id)
 
         # download records has not downloaded to local machine
         if len(not_downloaded_record_Ids) > 0:
             self.request_download_records(not_downloaded_record_Ids)
-        elif len(export_record_Ids) > 0: # or export records are in local machine
-            self.export_record(export_record_Ids)
+
+        # Open comment below to export records
+        # if len(export_record_Ids) > 0: # or export records are in local machine
+        #     self.export_record(export_record_Ids, license_ids)
 
     def on_export_record_done(self, *args, **kwargs):
         print('on_export_record_done: the successful record exporting as below:')
@@ -186,6 +186,14 @@ def main():
     
     # Don't need to create session in this case
     r = Records(your_app_client_id, your_app_client_secret, auto_create_session= False)
+
+    # As default, the Program will query records of your application.
+    # In the case, you want to query records created from other application (such as EmotivPRO). 
+    #   You need set license_id and application_id of the application
+    #   If set license_id without application_id. It will return records created from all applications use the license_id
+    #   If set both license_id and application_id. It will return records from the application has the application_id
+    r.license_id = ''
+    r.application_id =  ''
     
     r.start()
 
