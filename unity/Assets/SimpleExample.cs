11e4d52d000204fc0aeedb8add97beb2b2714a48
tung_tung <tungnguyen@emotiv.com>
2024-11-15 17:11:09 +0700

    add code to open webview

--- unity/Assets/SimpleExample.cs
+++ unity/Assets/SimpleExample.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using EmotivUnityPlugin;
 using UnityEngine.UI;
-
+#if UNITY_ANDROID
+using UnityEngine.Android;
+using Gpm.WebView;
+#endif
 public class SimpleExample : MonoBehaviour
 {
     
@@ -110,6 +113,36 @@ public class SimpleExample : MonoBehaviour
         }
     }
     #endif
+
+    // Popup default
+public void ShowUrlPopupDefault()
+{
+        string server = "cerebrum.emotivcloud.com";
+        string urlRequest = "https://" + server + "/api/oauth/authorize/" ;
+
+    GpmWebView.ShowUrl(
+        urlRequest,
+        new GpmWebViewRequest.Configuration()
+        {
+            style = GpmWebViewStyle.POPUP,
+            orientation = GpmOrientation.UNSPECIFIED,
+            isClearCookie = true,
+            isClearCache = true,
+            isNavigationBarVisible = true,
+            isCloseButtonVisible = true,
+            supportMultipleWindows = true,
+#if UNITY_IOS
+            contentMode = GpmWebViewContentMode.MOBILE,
+            isMaskViewVisible = true,
+#endif
+        },
+        // See the end of the code example
+        OnCallback,
+        new List<string>()
+        {
+            "USER_ CUSTOM_SCHEME"
+        });
+}
     
     void Start()
     {
@@ -151,6 +184,15 @@ public class SimpleExample : MonoBehaviour
             MessageLog.color = Color.black;
         }
         MessageLog.text = _eItf.MessageLog;
+
+        // check login status
+        if (_eItf.GetConnectToCortexState() == ConnectToCortexStates.Login_notYet) {
+            #if UNITY_ANDROID
+                UnityEngine.Debug.Log("ShowUrlPopupDefault");
+                ShowUrlPopupDefault();
+            #endif
+        }
+
         if (!_eItf.IsAuthorizedOK)
             return;
 
@@ -410,5 +452,73 @@ public class SimpleExample : MonoBehaviour
             _streams.Add("com");
         }
         return _streams;
+    }
+
+    private void OnCallback(
+    GpmWebViewCallback.CallbackType callbackType,
+    string data,
+    GpmWebViewError error)
+    {
+        Debug.Log("OnCallback: " + callbackType + " data: " + data + " error: " + error);
+        switch (callbackType)
+        {
+            case GpmWebViewCallback.CallbackType.Open:
+                if (error != null)
+                {
+                    Debug.LogFormat("Fail to open WebView. Error:{0}", error);
+                }
+                break;
+            case GpmWebViewCallback.CallbackType.Close:
+                if (error != null)
+                {
+                    Debug.LogFormat("Fail to close WebView. Error:{0}", error);
+                }
+                break;
+            case GpmWebViewCallback.CallbackType.PageStarted:
+                if (string.IsNullOrEmpty(data) == false)
+                {
+                    Debug.LogFormat("PageStarted Url : {0}", data);
+                }
+                break;
+            case GpmWebViewCallback.CallbackType.PageLoad:
+                if (string.IsNullOrEmpty(data) == false)
+                {
+                    Debug.LogFormat("Loaded Page:{0}", data);
+                }
+                break;
+            case GpmWebViewCallback.CallbackType.MultiWindowOpen:
+                Debug.Log("MultiWindowOpen");
+                break;
+            case GpmWebViewCallback.CallbackType.MultiWindowClose:
+                Debug.Log("MultiWindowClose");
+                break;
+            case GpmWebViewCallback.CallbackType.Scheme:
+                if (error == null)
+                {
+                    if (data.Equals("USER_ CUSTOM_SCHEME") == true || data.Contains("CUSTOM_SCHEME") == true)
+                    {
+                        Debug.Log(string.Format("scheme:{0}", data));
+                    }
+                }
+                else
+                {
+                    Debug.Log(string.Format("Fail to custom scheme. Error:{0}", error));
+                }
+                break;
+            case GpmWebViewCallback.CallbackType.GoBack:
+                Debug.Log("GoBack");
+                break;
+            case GpmWebViewCallback.CallbackType.GoForward:
+                Debug.Log("GoForward");
+                break;
+            case GpmWebViewCallback.CallbackType.ExecuteJavascript:
+                Debug.LogFormat("ExecuteJavascript data : {0}, error : {1}", data, error);
+                break;
+    #if UNITY_ANDROID
+            case GpmWebViewCallback.CallbackType.BackButtonClose:
+                Debug.Log("BackButtonClose");
+                break;
+    #endif
+        }
     }
 }
